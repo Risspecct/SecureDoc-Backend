8d0d0a7a88411c09cd8497e245b06867706d1e75
Risspecct <rishikgarg.dev@gmail.com>
2025-07-20 17:32:13 +0530

    Added security service to cleanly get current authenticated user details

--- src/main/java/users/rishik/SecureDoc/Security/Service/SecurityService.java
+++ src/main/java/users/rishik/SecureDoc/Security/Service/SecurityService.java
@@ -1,4 +1,17 @@
 package users.rishik.SecureDoc.Security.Service;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import users.rishik.SecureDoc.Security.Principals.UserPrincipal;
+
+@Service
 public class SecurityService {
+    public UserPrincipal getCurrentUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null && auth.getPrincipal() instanceof UserPrincipal) {
+            return (UserPrincipal) auth.getPrincipal();
+        }
+        throw new IllegalStateException("User not authenticated");
+    }
 }
